0180069a0400d66f6c01b41d03e9a6df845a3ba3
DarkStoorM <b.sieprawski@op.pl>
2017-02-01 14:08:33 +0100

    Disable error reporting

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
  * @author Bartosz "DarkstoorM" Sieprawski <b.sieprawski@op.pl>
  * @license MIT License
  */
-	
+
+// Disable the error reporting , so it won't get into our output.
+error_reporting(0);
+
 // BullyBot
 require ("bully.class.php");
 $bully = new BullyBot();
