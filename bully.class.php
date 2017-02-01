08ddd4bca490d94d09107671a4ade07b9c4d5260
DarkStoorM <b.sieprawski@op.pl>
2017-02-01 12:02:26 +0100

    Remove unnecessary user check

--- bully.class.php
+++ bully.class.php
@@ -22,19 +22,29 @@ class BullyBot {
 
 		$user = $_GET["u"];
 
-		// If there was a username sent
-		if (strlen(trim($user)) < 1 || strlen(trim($user)) > 25)
-			return false;
-		// If username is invalid according to Twitch Username rules
-		// It doesn't have to be that accurate anyway, there are still old users
-		// with 1-3 characters names
-		elseif (!preg_match("/^[a-z0-9_]{1,25}$/i", $user))
-			return false;
 		// It doesn't need any further validation as long as it matches the pattern
-		else {
-			$this->user = strip_tags($user);
+		if (preg_match("/^[a-z0-9_]{1,25}$/i", $user)) {
+			$this->user = $user;
 			return true;
-		}
+		} else
+			return false;
+	}
+
+	/**
+	 * Returns a random insult
+	 * @return string Random insult chosen from the list
+	 */
+	private function GetBully() {
+		// Set up a username variable instead for easier changes
+		$user = $this->user;
+		require ("bully.list.php");
+		
+		// Select a number between 0 and the number of bully list
+		$randomNumber = mt_rand(0, (count($bullyList) -1) );
+
+		// Pick the random insult
+		$bullyResponse = $bullyList[$randomNumber];
+		return $bullyResponse;
 	}
 
 	/**
@@ -54,22 +64,5 @@ class BullyBot {
 
 		// Echo out the insult with given username
 		echo $insult;
-	}
-
-	/**
-	 * Returns a random insult
-	 * @return string Random insult chosen from the list
-	 */
-	private function GetBully() {
-		// Set up a username variable instead for easier changes
-		$user = $this->user;
-		require ("bully.list.php");
-		
-		// Select a number between 0 and the number of bully list
-		$randomNumber = mt_rand(0, (count($bullyList) -1) );
-
-		// Pick the random insult
-		$bullyResponse = $bullyList[$randomNumber];
-		return $bullyResponse;
 	}
 }
